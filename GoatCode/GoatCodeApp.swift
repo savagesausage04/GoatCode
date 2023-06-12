25e4c7b9994df3390c13468404d45155043a1344
Kyle <kyleh1104@gmail.com>
2023-06-12 13:13:07 -0700

    Initial Commit

---
+++ GoatCode/GoatCodeApp.swift
@@ -0,0 +1,20 @@
+//
+//  GoatCodeApp.swift
+//  GoatCode
+//
+//  Created by Kyle Han on 6/12/23.
+//
+
+import SwiftUI
+
+@main
+struct GoatCodeApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
