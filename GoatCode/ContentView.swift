f596c7ef0452c81314ea30ae46586b0fbbac60e5
savagesausage04 <kyleh1104@gmail.com>
2023-06-28 21:59:03 -0700

    more UI
    tried new UI stuff, commented out old UI stuff

--- GoatCode/ContentView.swift
+++ GoatCode/ContentView.swift
@@ -6,11 +6,11 @@
 //
 
 import SwiftUI
-import CoreData
+
 
 struct ContentView: View {
     var body: some View {
-        ZStack {
+        /*ZStack {
             Color(.black).ignoresSafeArea()
             
             VStack {
@@ -24,9 +24,32 @@ struct ContentView: View {
                 
             }
         }
-        
-       
-        
+        */
+        ZStack {
+            RoundedRectangle(cornerRadius: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Corner Radius@*/10.0/*@END_MENU_TOKEN@*/)
+                .frame(height: 600.0)
+                .foregroundColor(Color.blue)
+                .padding(.all)
+            
+                
+            VStack(alignment: .leading, spacing: 20.0) {
+                
+                Text("Intro to Loops")            .font(/*@START_MENU_TOKEN@*/.largeTitle/*@END_MENU_TOKEN@*/).fontWeight(.semibold).padding(.horizontal)
+                
+                Text("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam et velit diam. Morbi pellentesque nisl sit amet tempus gravida. Suspendisse potenti. Suspendisse potenti. Aenean ultricies eros vitae tellus volutpat, a tempor mi iaculis. Sed rutrum a leo quis faucibus. Nunc vitae suscipit orci, in malesuada felis. Duis blandit interdum nisl.")
+                    .padding(.horizontal)
+                
+                Image("BlockCode")
+                    .resizable()
+                    .cornerRadius(10)
+                    .aspectRatio(contentMode: .fit)
+                    .padding(/*@START_MENU_TOKEN@*/.horizontal/*@END_MENU_TOKEN@*/)
+                
+                
+            }
+            .padding(.horizontal, 10.0)
+            
+        }
         
     }
 }
--- GoatCode/GoatCodeApp.swift
+++ GoatCode/GoatCodeApp.swift
@@ -3,18 +3,16 @@
 //  GoatCode
 //
 //  Created by Kyle Han on 6/12/23.
-// change
+
 
 import SwiftUI
 
 @main
 struct GoatCodeApp: App {
-    let persistenceController = PersistenceController.shared
 
     var body: some Scene {
         WindowGroup {
             ContentView()
-                .environment(\.managedObjectContext, persistenceController.container.viewContext)
         }
     }
 }
