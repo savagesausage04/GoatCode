43b37637d3214ab2be8d5d82bc8f0f73dcd8c4cb
savagesausage04 <kyleh1104@gmail.com>
2023-07-06 18:17:04 -0700

    Scroll
    added scroll feature to levels page

--- GoatCode/UnitPage.swift
+++ GoatCode/UnitPage.swift
@@ -9,22 +9,41 @@ import Foundation
 
 struct UnitPage: View {
     var body: some View {
-        
-        let colors: [Color] = [.red, .blue, .green, .orange, .purple]
-        let xPositions: [CGFloat] = [50, 100, 20, -20, 0]
-        
-        VStack(spacing: 40) {
-            ForEach(0..<5) { index in
-                Button(action: {
-                    // Add your button action code here
-                    print("Button \(index + 1) tapped!")
-                }) {
-                    Circle()
-                        .fill(colors[index])
-                        .frame(width: 100, height: 100)
-                        .offset(x: xPositions[index])
+        ScrollView(showsIndicators: false) {
+            LazyVStack {
+                let colors: [Color] = [.red, .blue, .green, .orange, .purple]
+                let colorss: [Color] = [.yellow, .blue, .green, .orange, .purple]
+                let xPositions: [CGFloat] = [50, 100, 20, -20, 0]
+                
+                VStack(spacing: 40) {
+                    ForEach(0..<5) { index in
+                        Button(action: {
+                            // Add your button action code here
+                            print("Button \(index + 1) tapped!")
+                        }) {
+                            Circle()
+                                .fill(colors[index])
+                                .frame(width: 100, height: 100)
+                                .offset(x: xPositions[index])
+                        }
+                        
+                    }
                 }
                 
+                VStack(spacing: 40) {
+                    ForEach(0..<5) { index in
+                        Button(action: {
+                            // Add your button action code here
+                            print("Button \(index + 1) tapped!")
+                        }) {
+                            Circle()
+                                .fill(colorss[index])
+                                .frame(width: 100, height: 100)
+                                .offset(x: xPositions[index])
+                        }
+                        
+                    }
+                }
             }
         }
     }
