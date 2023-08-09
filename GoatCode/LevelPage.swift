cb9aed100509ba664d37771a0334f26f63b85e9b
lucy-x <lucyclxu@gmail.com>
2023-08-08 18:43:42 -0700

    jdkfla;wlkdjf

--- GoatCode/LevelPage.swift
+++ GoatCode/LevelPage.swift
@@ -17,12 +17,6 @@ struct LevelPage: View {
          ZStack {
          Color("lightBrown")
                  .ignoresSafeArea()
-         
-         RoundedRectangle(cornerRadius: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Corner Radius@*/10.0/*@END_MENU_TOKEN@*/)
-             .frame(height: 550.0)
-             .foregroundColor(Color.brown)
-             .padding(.all)
-             .offset(y: -90)
             
          Image("chevron-forward-outline")
                   .resizable()
@@ -52,15 +46,7 @@ struct LevelPage: View {
                  .offset(x: 10, y: 20)
                  .foregroundColor(.white)
          }
-         Text("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam et velit diam. Morbi pellentesque nisl sit amet tempus gravida. Suspendisse potenti. Suspendisse potenti. Aenean ultricies eros vitae tellus volutpat, a tempor mi iaculis. Sed rutrum a leo quis faucibus. Nunc vitae suscipit orci, in malesuada felis. Duis blandit interdum nisl.")
-         .padding(.horizontal)
-             
-         
-         Image("BlockCode")
-         .resizable()
-         .cornerRadius(10)
-         .aspectRatio(contentMode: .fit)
-         .padding(/*@START_MENU_TOKEN@*/.horizontal/*@END_MENU_TOKEN@*/)
+            
          
          Image("bubble")
                  .resizable()
